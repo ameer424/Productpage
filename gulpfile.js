f9a065c356968aa41df9938d4d0e580510d3bad5
ameer424 <65288284+ameer424@users.noreply.github.com>
2021-05-27 00:43:01 +0300

    remove button.js from bootstrap components

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ function defaultTask() {
         'node_modules/bootstrap/js/dist/dom/selector-engine.js',
         'node_modules/bootstrap/js/dist/dom/event-handler.js',
         'node_modules/bootstrap/js/dist/base-component.js',
-        'node_modules/bootstrap/js/dist/button.js',
+        // 'node_modules/bootstrap/js/dist/button.js',
         'node_modules/bootstrap/js/dist/dropdown.js',
         'node_modules/bootstrap/js/dist/collapse.js',
         'node_modules/toastr/toastr.js',
